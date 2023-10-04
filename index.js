025b55153b641ee25e2d3ac999953295290ba79f
Ricardo Guerra <RikkuG491@gmail.com>
2023-10-04 12:58:58 -0400

    Added solution code

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+
+const mainElement = document.getElementById('main')
+mainElement.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = "Bob is the champion"
+
+document.body.appendChild(newHeader)
+
+// Extra stuff
+
+const divElement = document.createElement('div')
+divElement.id = "container"
+document.body.appendChild(divElement)
+
+divElement.innerHTML = "Hello\nWorld"
