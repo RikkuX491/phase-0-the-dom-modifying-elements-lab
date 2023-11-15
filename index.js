b929af5ee75d0049f9860b8d8eb471f410f933ec
Ricardo Guerra <RikkuG491@gmail.com>
2023-11-15 15:16:05 -0500

    Added solution code

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.getElementById('main').remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+// newHeader.setAttribute("id", "victory")
+newHeader.textContent = 'Bob is the champion'
+
+// document.body.appendChild(newHeader);
