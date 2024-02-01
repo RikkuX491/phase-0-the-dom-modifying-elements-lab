ea621a3af2782b804a93b1a180345e59a6cae926
Ricardo Guerra <RikkuG491@gmail.com>
2024-02-01 16:33:48 -0500

    Added solution code

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+
+// Test # 1 - solution code
+const mainElement = document.querySelector('main#main')
+mainElement.remove()
+
+// Test # 2 - solution code
+const newHeader = document.createElement('h1')
+
+// Test # 3 - solution code
+newHeader.id = 'victory'
+// newHeader.setAttribute('id', 'victory')
+
+// Test # 4 - solution code
+newHeader.textContent = "Drew is the champion"
+
+// Bonus: Append the newHeader h1 element to document.body
+document.body.appendChild(newHeader)
