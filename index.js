517b878039fc59e7076c9cb966be8f4de03f392b
Ricardo Guerra <RikkuG491@gmail.com>
2024-08-28 14:28:06 -0400

    Added solution code

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// Solution code for Test # 1
+const mainElement = document.getElementById('main')
+mainElement.remove()
+
+// Solution code for Test # 2
+const newHeader = document.createElement('h1')
+
+// Solution code for Test # 3
+newHeader.id = 'victory'
+
+// Solution code for Test # 4
+newHeader.textContent = "Alice is the champion"
